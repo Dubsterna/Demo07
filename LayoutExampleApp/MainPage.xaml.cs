061efaf22b963cac4f3bdf259a59671f76c0066f
Mika <k2763@student.jamk.fi>
2016-02-24 10:37:29 +0200

    Initial commit

---
+++ LayoutExampleApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LayoutExampleApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            //Piirretään UI XAML-koodista
+            this.InitializeComponent();
+
+            //ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Name:" + nameTextBox.Text);
+            Debug.WriteLine("Email:" + emailTextBox.Text);
+            //Password
+            if ((bool)userRadioButton.IsChecked) Debug.WriteLine("Normal User");
+            if ((bool)adminRadioButton.IsChecked) Debug.WriteLine("Admin");
+
+        }
+    }
+}
